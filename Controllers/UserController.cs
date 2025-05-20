3be59ad083fd9dedc08d389de6797356880e21f5
Robert Takacs <RoTak@protonmail.com>
2025-05-20 16:11:19 +0300

    FIX MERGE ERROR. After PR merge

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -222,57 +222,5 @@ public class UserController : Controller
         public string Theme { get; set; }
     }
     
-    public async Task<IActionResult> MyImages()
-    {
-        if (User.Identity?.IsAuthenticated == true)
-        {
-            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
-
-            var uploaded = await _context.UploadFiles
-                .Where(f => f.UserId == userId)
-                .ToListAsync();
-
-            var tasks = await _context.ImageTasks
-                .Include(t => t.File)
-                .Include(t => t.ImageTool)
-                .Where(t => t.File.UserId == userId)
-                .ToListAsync();
-
-            var grouped = tasks
-                .GroupBy(t => t.ImageTool.Name)
-                .ToDictionary(g => g.Key, g => g.ToList());
-
-            var model = new MyImagesViewModel
-            {
-                UploadedFiles = uploaded,
-                TasksByTool = grouped
-            };
-
-            return View(model);
-        }
-        return RedirectToAction("Index", "Home");
-    }
     
-    [HttpPost]
-    public async Task<IActionResult> DeleteUpload(int id)
-    {
-        var file = await _context.UploadFiles.FindAsync(id);
-        if (file != null)
-        {
-            _context.UploadFiles.Remove(file);
-            await _context.SaveChangesAsync();
-        }
-        return RedirectToAction("MyImages");
-    }
-    
-    [HttpPost]
-    public async Task<IActionResult> DeleteTask(int id)
-    {
-        var task = await _context.ImageTasks.FindAsync(id);
-        if (task != null)
-        {
-            _context.ImageTasks.Remove(task);
-        }
-        return RedirectToAction("MyImages");
-    }
 }
